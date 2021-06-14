37bfea485c1dedd30a44f19c1d654f6283615513
alex <azalexanderzhang@gmail.com>
2021-06-14 10:29:54 -0400

    creating users using github id

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,20 +1,6 @@
 const { UserModel } = require('../models');
 
 class UserController {
-  createUser(req, res, next) {
-    return UserModel.create({
-      ...req.body,
-      githubId: res.locals.githubUser ? res.locals.githubUser.id : null,
-    })
-      .then((result) => {
-        console.log(result);
-        next();
-      })
-      .catch((err) => {
-        next(err);
-      });
-  }
-
   getUserById(req, res, next) {
     UserModel.getById(req.params.id)
       .then((data) => {
@@ -29,6 +15,59 @@ class UserController {
 
         res.locals.user = user;
 
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+
+  getUserByGithubId(req, res, next) {
+    UserModel.getByGithubId(res.locals.githubUser.id)
+      .then((data) => {
+        const user = data.rows[0];
+
+        if (!user) {
+          throw {
+            statusCode: 400,
+            message: 'user not found',
+          };
+        }
+
+        res.locals.user = user;
+
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+
+  createUser(req, res, next) {
+    return UserModel.create({
+      ...req.body,
+      githubId: res.locals.githubUser ? res.locals.githubUser.id : null,
+    })
+      .then((result) => {
+        next();
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+
+  createUserWithGithub(req, res, next) {
+    return UserModel.getByGithubId(res.locals.githubUser.id)
+      .then((result) => {
+        if (result.rows.length === 1) {
+          return;
+        }
+        return UserModel.create({
+          ...req.body,
+          githubId: res.locals.githubUser ? res.locals.githubUser.id : null,
+        });
+      })
+      .then(() => {
         next();
       })
       .catch((err) => {
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,14 +1,15 @@
 const { Router } = require('express');
-const { authController } = require('../controllers');
+const { authController, userController } = require('../controllers');
 
 const router = Router();
 
 router.post(
   '/github',
-  authController.getGithubToken,
+  authController.createGithubToken,
   authController.getGithubUser,
+  userController.createUserWithGithub,
+  userController.getUserByGithubId,
   (req, res) => {
-    console.log(res.locals.githubUser);
     res.status(200).json({
       githubToken: res.locals.githubToken,
     });
